f85a23a2953d2cca7c49757c8799470252eae5a2
Stasia Kroeger <stasiakroeger@gmail.com>
2016-06-21 19:32:36 +0000

    Done.

--- lib/mothers_day.rb
+++ lib/mothers_day.rb
@@ -1,6 +1,11 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-
-
+def mothers_day
+  return "Happy Mother's Day, Mom!"
+end
 
 ## Call your method below - use puts to see it in the terminal!
+def mothers_day(name, defaultname = "Mom")
+  return "Happy Mother's Day, #{name}!"
+end
 
+puts mothers_day("Mom")
